64cdb85a483d7527c276e238de5d4d550129d733
andrey-manakov <manakov@gmail.com>
2025-06-14 00:50:43 +0300

    env

--- invevent/web/main.py
+++ invevent/web/main.py
@@ -9,6 +9,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from invevent.models import Event
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///db.sqlite")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
